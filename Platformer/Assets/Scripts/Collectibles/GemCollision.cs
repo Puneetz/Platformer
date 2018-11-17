2d15e3beaab9deaca7a281637a022d9ef77f0178
Aleksbgbg <kozichka01@abv.bg>
2018-11-17 20:06:21 +0000

    [Collectibles] Add several gems which can be picked up, with feedback animation.

---
+++ Platformer/Assets/Scripts/Collectibles/GemCollision.cs
@@ -0,0 +1,24 @@
+﻿namespace Scripts.Collectibles
+{
+    using UnityEngine;
+
+    public class GemCollision : MonoBehaviour
+    {
+        private Animator _gemAnimator;
+
+        public void OnDestroyAnimationFinished()
+        {
+            Destroy(gameObject);
+        }
+
+        private void Awake()
+        {
+            _gemAnimator = gameObject.GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            _gemAnimator.SetBool("IsCollected", true);
+        }
+    }
+}
